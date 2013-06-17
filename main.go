57f6b36e265fb1db2d250ea28a384f5ff38f2638
Alexandr Parfenov <demoh748@gmail.com>
2013-06-17 17:42:08 +0700

    Added command line argument parsing for ROM opening
    Signed-off-by: Alexandr Parfenov <demoh748@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+)
+
 func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		fmt.Printf("You must send ROM name. Example\n chipigo maze.rom\n")
+	}
+	_, err := ioutil.ReadFile(flag.Arg(0))
+	if err != nil {
+		fmt.Printf("%s\n", err.Error())
+		return
+	}
 	console := CHIP8Console_i(new(CHIP8Console))
-	console.init("test.rom")
+	console.init(flag.Arg(0))
 	console.loop()
 }
