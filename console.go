a7c56f94d6199a12789bc57fa50914b14a059f64
Alexander Parfenov <asp437@gmail.com>
2015-07-24 21:11:54 +0300

    Use new versions of github.com/go-gl/gl and github.com/go-gl/glfw

--- console.go
+++ console.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/go-gl/glfw"
+	"github.com/go-gl/glfw/v3.1/glfw"
 	"time"
 )
 
@@ -12,11 +12,12 @@ type CHIP8Console_i interface {
 }
 
 type CHIP8Console struct {
-	mem   CHIP8Memory_i
-	cpu   CHIP8CPU_i
-	gpu   CHIP8GPU_i
-	input CHIP8Input_i
-	sound CHIP8Sound_i
+	mem    CHIP8Memory_i
+	cpu    CHIP8CPU_i
+	gpu    CHIP8GPU_i
+	input  CHIP8Input_i
+	sound  CHIP8Sound_i
+	window *glfw.Window
 }
 
 func (console *CHIP8Console) init(str string) {
@@ -29,28 +30,28 @@ func (console *CHIP8Console) init(str string) {
 	console.cpu.init()
 	console.mem.init()
 	console.mem.read_rom(str)
-	console.gpu.init()
-	console.input.init()
+	console.window = console.gpu.init()
+	console.input.init(console.window)
 	console.sound.init()
 }
 
 func (console *CHIP8Console) loop() {
-	cycles_per_second := 1000.0
-	one_cycle_in_ms := 1000.0 / cycles_per_second
-	var last_time, new_time, unprocessed float64
-	last_time = glfw.Time() * 1000
-	new_time = glfw.Time() * 1000
-	unprocessed = 0.0
+	cycles_per_second := 120.0
+	one_cycle_in_ms := 1.0 / cycles_per_second
+	last_time := glfw.GetTime()
+	new_time := glfw.GetTime()
+	unprocessed := 0.0
 	for {
 		last_time = new_time
-		new_time = glfw.Time() * 1000
+		new_time = glfw.GetTime()
 		unprocessed += new_time - last_time
 		glfw.PollEvents()
-		if glfw.Key(glfw.KeyEsc) == glfw.KeyPress || glfw.WindowParam(glfw.Opened) == 0 {
+		if console.window.GetKey(glfw.KeyEscape) == glfw.Press || console.window.ShouldClose() {
 			break
 		}
 		for unprocessed > one_cycle_in_ms {
-			if glfw.Key(glfw.KeyEsc) == glfw.KeyPress || glfw.WindowParam(glfw.Opened) == 0 {
+			glfw.PollEvents()
+			if console.window.GetKey(glfw.KeyEscape) == glfw.Press || console.window.ShouldClose() {
 				break
 			}
 			console.tick()
--- console_io.go
+++ console_io.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"github.com/go-gl/gl"
-	"github.com/go-gl/glfw"
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
 )
 
 type CHIP8GPU_i interface {
 	clear_screen()
 	render()
-	init()
+	init() *glfw.Window
 	draw_line8(x, y int8, line uint8) Registr // Return new value of VF
 }
 
 type CHIP8GPU struct {
-	pic   [][]uint8 // Screen can display only black and some 1 color
-	w, h  int       // width and height of screen
-	color uint32    // color for filled pixels
+	pic    [][]uint8 // Screen can display only black and some 1 color
+	w, h   int       // width and height of screen
+	color  uint32    // color for filled pixels
+	window *glfw.Window
 } // Not full implemented yet
 
 // The interpreter reads n bytes from memory, starting at the address stored in I.
@@ -62,8 +63,9 @@ func (gpu *CHIP8GPU) clear_screen() {
 func (gpu *CHIP8GPU) render() {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.LoadIdentity()
-	for x := 0; x < gpu.w; x++ {
-		for y := 0; y < gpu.h; y++ {
+	var x, y int32
+	for x = 0; x < int32(gpu.w); x++ {
+		for y = 0; y < int32(gpu.h); y++ {
 			if gpu.pic[x][y] != 0 {
 				gl.Begin(gl.QUADS)
 				gl.Color3ub(uint8(gpu.color&0xFF0000>>16), uint8(gpu.color&0x00FF00>>8), uint8(gpu.color&0x0000FF))
@@ -75,10 +77,10 @@ func (gpu *CHIP8GPU) render() {
 			}
 		}
 	}
-	glfw.SwapBuffers()
+	gpu.window.SwapBuffers()
 }
 
-func (gpu *CHIP8GPU) init() {
+func (gpu *CHIP8GPU) init() *glfw.Window {
 	gpu.w = 64
 	gpu.h = 32
 	gpu.pic = make([][]uint8, gpu.w)
@@ -91,10 +93,17 @@ func (gpu *CHIP8GPU) init() {
 	gpu.color = 0x11FF11 // Some kind of green
 
 	glfw.Init()
-	glfw.OpenWindow(640, 320, 0, 0, 0, 0, 0, 0, glfw.Windowed)
-	gl.Init()
 
-	gl.ShadeModel(gl.SMOOTH)
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 2)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	gpu.window, _ = glfw.CreateWindow(640, 320, "CHIPIGO", nil, nil)
+	gpu.window.MakeContextCurrent()
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	// gl.ShadeModel(gl.SMOOTH)
 	gl.ClearColor(0.1, 0.1, 0.1, 1.0)
 	gl.ClearDepth(1.0)
 	gl.MatrixMode(gl.PROJECTION)
@@ -102,6 +111,7 @@ func (gpu *CHIP8GPU) init() {
 	gl.Ortho(0, 64, 32, 0, -1, 1)
 	gl.MatrixMode(gl.MODELVIEW)
 	gl.LoadIdentity()
+	return gpu.window
 }
 
 type CHIP8Sound_i interface {
@@ -125,17 +135,19 @@ func (sound *CHIP8Sound) turn_beep(val bool) {
 func (sound *CHIP8Sound) tick() {}
 
 type CHIP8Input_i interface {
-	init()
+	init(*glfw.Window)
 	is_pressed(key uint8) bool
 	tick()
 }
 
 type CHIP8Input struct {
-	keys int
+	keys   int
+	window *glfw.Window
 }
 
-func (input *CHIP8Input) init() {
+func (input *CHIP8Input) init(window *glfw.Window) {
 	input.keys = -255
+	input.window = window
 }
 
 func (input *CHIP8Input) is_pressed(key uint8) bool {
@@ -144,37 +156,37 @@ func (input *CHIP8Input) is_pressed(key uint8) bool {
 
 func (input *CHIP8Input) tick() {
 	input.keys = -255
-	if glfw.Key('1') == glfw.KeyPress {
+	if input.window.GetKey(glfw.Key1) == glfw.Press {
 		input.keys = 0x1
-	} else if glfw.Key('2') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.Key2) == glfw.Press {
 		input.keys = 0x2
-	} else if glfw.Key('3') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.Key3) == glfw.Press {
 		input.keys = 0x3
-	} else if glfw.Key('4') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.Key4) == glfw.Press {
 		input.keys = 0xC
-	} else if glfw.Key('Q') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyQ) == glfw.Press {
 		input.keys = 0x4
-	} else if glfw.Key('W') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyW) == glfw.Press {
 		input.keys = 0x5
-	} else if glfw.Key('E') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyE) == glfw.Press {
 		input.keys = 0x6
-	} else if glfw.Key('R') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyR) == glfw.Press {
 		input.keys = 0xD
-	} else if glfw.Key('A') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyA) == glfw.Press {
 		input.keys = 0x7
-	} else if glfw.Key('S') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyS) == glfw.Press {
 		input.keys = 0x8
-	} else if glfw.Key('D') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyD) == glfw.Press {
 		input.keys = 0x9
-	} else if glfw.Key('F') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyF) == glfw.Press {
 		input.keys = 0xE
-	} else if glfw.Key('Z') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyZ) == glfw.Press {
 		input.keys = 0xA
-	} else if glfw.Key('X') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyX) == glfw.Press {
 		input.keys = 0x0
-	} else if glfw.Key('C') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyC) == glfw.Press {
 		input.keys = 0xB
-	} else if glfw.Key('V') == glfw.KeyPress {
+	} else if input.window.GetKey(glfw.KeyV) == glfw.Press {
 		input.keys = 0xF
 	}
 }
